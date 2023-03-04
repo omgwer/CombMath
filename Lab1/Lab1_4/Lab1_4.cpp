77be3cd41f1674dde2226c930c1e6cc373caa04d
aleksandr <hersgory2134@gmail.com>
2023-03-04 22:21:47 +0300

    feat: add lab 1.5

--- Lab1/Lab1_4/Lab1_4.cpp
+++ Lab1/Lab1_4/Lab1_4.cpp
@@ -1,53 +1,142 @@
+#include <algorithm>
 #include <fstream>
+#include <iostream>
 #include <vector>
+#include <boost/timer.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
+struct Result;
 using namespace std;
-
 vector<vector<int>> GenerateFullGraph(int);
+vector<Result> SearchAllPathLong(const vector<vector<int>> &graphInMatrix, int graphSize, size_t& iterablesCount);
+size_t GetRoadLongByRoadPath(const vector<size_t> &roadPath, const vector<vector<int>> &graphInMatrix);
+void PrintResult(const double elapsedTime, const size_t iterablesCount,const unsigned short vectorSize);
+
+struct Result
+{
+    std::vector<size_t> roadPath;
+    size_t roadLong;
+};
 
 /*1.4. [# 25] Задача коммивояжера.
 	Для графа, заданного матрицей смежности найти гамильтонов цикл минимальной суммарной стоимости.Предполагается, что граф задан матрицей смежности.
 	Решить задачу для 𝑁=10, 𝑁=15, где 𝑁 – количество вершин в графе. Оценить время работы программы для входа 𝑁=20 и 𝑁=50.
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int m_graphSize = 5;
-	vector<vector<int>> graph = GenerateFullGraph(m_graphSize);
-	
-	return 0;
+    size_t iterablesCount = 0;
+    constexpr int graphSize = 10;
+    const vector<vector<int>> graphInMatrix = GenerateFullGraph(graphSize);
+    boost::timer timer;
+    timer.restart();
+    const vector<Result> allRoadResults = SearchAllPathLong(graphInMatrix, graphSize, iterablesCount);
+    Result shortestRoadOnTheAllRoads;
+    shortestRoadOnTheAllRoads.roadLong = INT32_MAX;
+
+    for (const auto &allRoadResult : allRoadResults)
+    {
+        if (allRoadResult.roadLong < shortestRoadOnTheAllRoads.roadLong)
+            shortestRoadOnTheAllRoads = allRoadResult;
+    }
+    cout << "Short road is : ";
+    for (const auto roadPathElement : shortestRoadOnTheAllRoads.roadPath)
+    {
+        cout << roadPathElement << " ";
+    }
+    cout << endl <<"Road long is : " << shortestRoadOnTheAllRoads.roadLong << endl;
+    PrintResult(timer.elapsed(), iterablesCount, graphSize);
+    return 1;
 }
 
 vector<vector<int>> GenerateFullGraph(int matrixSize)
 {
-	std::ofstream out("fullGraph.txt");
-	out.clear();
-	for (int j = 1; j <= matrixSize; j++)
-	{
-		for (int i = 1; i <= matrixSize; i++)
-		{
-			if (j < i)
-			{
-				out << j << " " << i << " " << (rand() % 50 + 1);
-				if (j == (matrixSize - 1) && i == (matrixSize))
-				{
-					continue;
-				}
-				out << std::endl;
-			}
-		}
-	}
-	out.close();
-	matrixSize++; // нужно для работы с нулями
-	vector<vector<int>> matrix(matrixSize, vector<int>(matrixSize, INT32_MAX));
-	ifstream in("fullGraph.txt");
-	if (in.is_open()) {
-		int x = 0, y = 0, weight = 0;
-		while (!in.eof()) {  //init array
-			in >> x >> y >> weight;
-			matrix[x][y] = weight;
-			matrix[y][x] = weight;
-		}
-	}
-	in.close();
-	return matrix;
+    std::ofstream out("fullGraph.txt");
+    out.clear();
+    for (int j = 1; j <= matrixSize; j++)
+    {
+        for (int i = 1; i <= matrixSize; i++)
+        {
+            if (j < i)
+            {
+                out << j << " " << i << " " << (rand() % 50 + 1);
+                if (j == (matrixSize - 1) && i == (matrixSize))
+                {
+                    continue;
+                }
+                out << std::endl;
+            }
+        }
+    }
+    out.close();
+    matrixSize++; // нужно для работы с нулями
+    vector<vector<int>> matrix(matrixSize, vector<int>(matrixSize, INT32_MAX));
+    ifstream in("fullGraph.txt");
+    if (in.is_open())
+    {
+        int x = 0, y = 0, weight = 0;
+        while (!in.eof())
+        {
+            //init array
+            in >> x >> y >> weight;
+            matrix[x][y] = weight;
+            matrix[y][x] = weight;
+        }
+    }
+    else
+    {
+        throw exception("Error in open file");
+    }
+    in.close();
+    return matrix;
+}
+
+vector<Result> SearchAllPathLong(const vector<vector<int>> &graphInMatrix, const int graphSize, size_t& iterablesCount)
+{
+    vector<Result> allRoadResult;
+    vector<size_t> roadPath;
+    for (int i = 1; i <= graphSize; i++)
+        roadPath.push_back(i);
+    do
+    {
+        iterablesCount++;
+        allRoadResult.push_back({roadPath, GetRoadLongByRoadPath(roadPath, graphInMatrix)});
+    }
+    while (std::next_permutation(roadPath.begin(), roadPath.end()));
+    return allRoadResult;
+}
+
+size_t GetRoadLongByRoadPath(const vector<size_t> &roadPath, const vector<vector<int>> &graphInMatrix)
+{
+    size_t longOfTheRoad = 0;
+    for (size_t i = 0; i < roadPath.size(); i++)
+    {
+        if (i + 1 < roadPath.size())
+            longOfTheRoad += graphInMatrix[roadPath[i]][roadPath[i + 1]];
+        else
+            longOfTheRoad += graphInMatrix[roadPath[i]][roadPath[1]];
+    }  
+    return longOfTheRoad;
+}
+
+boost::multiprecision::cpp_int Factorial(const int number)
+{
+    boost::multiprecision::cpp_int num = 1;
+    for (int i = 1; i <= number; i++)
+        num = num * i;
+    return num;
+}
+
+void PrintResult(const double elapsedTime, const size_t iterablesCount,const unsigned short vectorSize)
+{
+    std::cout << "Permutation for vector size = " << vectorSize << ", generate in " << round(elapsedTime * 100) / 100.0 << " seconds." << std::endl;
+    std::cout << "Iteration count = " << iterablesCount << ".\n";
+    const boost::multiprecision::cpp_dec_float_50 oneIterableTime = elapsedTime/ iterablesCount;
+    std::cout << "Average for one Iteration = " << oneIterableTime << std::endl;
+	
+    std::cout << "Average for vector size = 10 is " << oneIterableTime *  Factorial(10).convert_to<boost::multiprecision::cpp_dec_float_50>() << " seconds." << std::endl;
+    std::cout << "Average for vector size = 15 is " << oneIterableTime *  Factorial(15).convert_to<boost::multiprecision::cpp_dec_float_50>() << " seconds." << std::endl;
+    std::cout << "Average for vector size = 20 is " << oneIterableTime *  Factorial(20).convert_to<boost::multiprecision::cpp_dec_float_50>() << " seconds." << std::endl;
+    std::cout << "Average for vector size = 50 is " << oneIterableTime * Factorial(50).convert_to<boost::multiprecision::cpp_dec_float_50>() << " seconds." << std::endl;
+    std::cout << "Average for vector size = 100 is " << oneIterableTime * Factorial(100).convert_to<boost::multiprecision::cpp_dec_float_50>() << " seconds." << std::endl;	
 }
--- Lab1/Lab1_5/Lab1_5.cpp
+++ Lab1/Lab1_5/Lab1_5.cpp
@@ -2,36 +2,66 @@
 #include <iostream>
 #include <vector>
 
-size_t GeneratePermutationWithNarayanaAlgorithm(unsigned short vectorSize);
-
+using namespace std;
+vector<vector<size_t>> GetCombination(size_t digitsCount, size_t maxDigit);
+size_t VectorSumma(vector<size_t> &vector);
 /* 1.5. [# 15]
 	Реализовать алгоритм порождения композиции положительного числа 𝑛 в последовательность положительных целых чисел {𝑧_1, 𝑧_2, …, 𝑧_𝑘 \}, 𝑧_1+𝑧_2+…+𝑧_𝑘=𝑛.
-	Здесь учитывается порядок чисел 𝑧_𝑖 и 𝑧_𝑖>0. */
+	Здесь учитывается порядок чисел 𝑧_𝑖 и 𝑧_𝑖>0.
+Понимание задачи. Должно быть так: вводишь число (например 5)
+Программа должна выдать все перестановки, сумма которых равна 5
+Пример: задача, которую писали на доске. Про яблоки, какими вариантами их можно поделить
+*/
 int main(int argc, char *argv[])
 {
     constexpr unsigned short vectorSize = 5;
-   
-   // добавить алгоритм сочетания  ( не нарайяна)  
+
+    // добавить алгоритм сочетания  ( не нарайяна)  
     //    6 slide
-    
-    GeneratePermutationWithNarayanaAlgorithm(vectorSize);
+    vector<vector<size_t>> test = GetCombination(2, 4);
     return 0;
 }
 
-size_t GeneratePermutationWithNarayanaAlgorithm(const unsigned short vectorSize)
+/**  
+ * Возвращает все сочетания в лексикографическом порядке, для {3,5} - пары будут 123 124 125 134 etc
+ * @param digitsCount - количество цифр в сочетании
+ * @param maxDigit - максимальное число
+ */
+vector<vector<size_t>> GetCombination(const size_t digitsCount, const size_t maxDigit)
 {
-    size_t iterablesCount = 0;
-    std::vector<size_t> result;
-    for (int i = 1; i <= vectorSize; i++)
-        result.push_back(i);
-    std::sort(result.begin(), result.end());
-    do
+    vector<size_t> combination(digitsCount + 1, 0);
+    vector<vector<size_t>> combinationResult;
+    combination[0] = -1;
+    for (size_t i = 1; i <= digitsCount; i++)
     {
-        std::cout << "Result vector: ";
-        copy(result.begin(), result.end(), std::ostream_iterator<size_t>(std::cout, " "));
-        iterablesCount++;
-        std::cout << std::endl;
+        combination[i] = i;
     }
-    while (std::next_permutation(result.begin(), result.end()));
-    return iterablesCount;
+    size_t m = 1;
+    while (m != 0)
+    {
+        combinationResult.push_back(combination);
+        copy(combination.begin() + 1, combination.end(), std::ostream_iterator<size_t>(std::cout, " "));
+        cout << endl;
+        m = digitsCount;
+        while (combination[m] == maxDigit - digitsCount + m)
+            m--;
+
+        combination[m] = combination[m] + 1;
+
+        for (size_t i = m + 1; i <= digitsCount; i++)
+        {
+            combination[i] = combination[i - 1] + 1;
+        }
+    }
+    return combinationResult;
+}
+
+size_t VectorSumma(vector<size_t> &vector)
+{
+    size_t vectorSum = 0;
+
+    // тут единица так как игнорирует 0 элемент вектора, там мусор
+    for (size_t i = 1; i < vector.size(); i++)
+        vectorSum += vector[i];
+    return vectorSum;
 }
