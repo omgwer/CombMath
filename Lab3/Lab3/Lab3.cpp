959264a323fde6be2c54627d09ac18451089d078
Alexandr Kislitsyn <63747048+omgwer@users.noreply.github.com>
2023-05-23 19:28:26 +0300

    feat: add description for lab 3

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -16,63 +16,72 @@ using namespace std;
 
 /*
 Функция для вычисления определителя матрицы
-
-Функция determinant вычисляет определитель матрицы laplacian с помощью рекурсивного алгоритма.
-1) В начале функции проверяется базовый случай. Если размер матрицы graphNodeCountMinisOne равен 1, то это означает, что матрица имеет размер 1x1,
-и мы просто возвращаем единственный элемент этой матрицы как определитель.
-2) Затем проверяется случай, когда размер матрицы graphNodeCountMinisOne равен 2. В этом случае мы используем формулу для вычисления определителя матрицы 2x2.
-3) В остальных случаях (когда размер матрицы больше 2), мы создаем подматрицу submat, которая является матрицей размера (graphNodeCountMinisOne - 1) x (graphNodeCountMinisOne - 1).
-Мы идем по каждому столбцу матрицы laplacian и рекурсивно вызываем функцию determinant, передавая подматрицу submat, удаляя текущий столбец и первую строку.
-4) Для каждого столбца k мы вычисляем минор laplacian[0][k] (первый элемент первой строки и k-ый столбец) и умножаем его на соответствующий алгебраический дополнительный знак (k % 2 == 0 ? 1 : -1).
-Затем рекурсивно вызываем функцию determinant для подматрицы submat, умноженной на минор, и добавляем результат к общему определителю det.
-5) В конце функция возвращает вычисленное значение определителя det.
-Таким образом, рекурсивно разбивая матрицу на подматрицы меньшего размера, алгоритм вычисляет определитель матрицы Лапласа графа, что позволяет найти количество остовных деревьев в графе.
+рекурсивно разбивая матрицу на подматрицы меньшего размера, алгоритм вычисляет определитель матрицы графа,
+что позволяет найти количество остовных деревьев в графе.
 */
-int determinant(vector<vector<int>>& laplacian, int graphNodeCountMinisOne) {
+int determinant(vector<vector<int>>& laplacian, int graphNodeCountMinisOne)
+{
     int det = 0;
-
-    if (graphNodeCountMinisOne == 1) {
+    //Если размер матрицы graphNodeCountMinisOne равен 1, то это означает, что матрица имеет размер 1x1,
+    //и мы просто возвращаем единственный элемент этой матрицы как определитель.
+    if (graphNodeCountMinisOne == 1)
         return laplacian[0][0];
-    } else if (graphNodeCountMinisOne == 2) {
+    // размер матрицы graphNodeCountMinisOne равен 2. В этом случае мы используем формулу для вычисления определителя матрицы 2x2.
+    if (graphNodeCountMinisOne == 2)
         return laplacian[0][0] * laplacian[1][1] - laplacian[0][1] * laplacian[1][0];
-    } else {
-        vector<vector<int>> submat(graphNodeCountMinisOne - 1, vector<int>(graphNodeCountMinisOne - 1));
-        for (int k = 0; k < graphNodeCountMinisOne; k++) {
-            int subi = 0;
-            for (int i = 1; i < graphNodeCountMinisOne; i++) {
-                int subj = 0;
-                for (int j = 0; j < graphNodeCountMinisOne; j++) {
-                    if (j != k) {
-                        submat[subi][subj] = laplacian[i][j];
-                        subj++;
-                    }
+
+    // В остальных случаях (когда размер матрицы больше 2), мы создаем подматрицу submat, которая является матрицей размера (graphNodeCountMinisOne - 1) x (graphNodeCountMinisOne - 1).
+    // Мы идем по каждому столбцу матрицы laplacian и рекурсивно вызываем функцию determinant, передавая подматрицу submat, удаляя текущий столбец и первую строку.
+    vector<vector<int>> submat(graphNodeCountMinisOne - 1, vector<int>(graphNodeCountMinisOne - 1));
+    for (int k = 0; k < graphNodeCountMinisOne; k++)
+    {
+        int subi = 0;
+        for (int i = 1; i < graphNodeCountMinisOne; i++)
+        {
+            int subj = 0;
+            for (int j = 0; j < graphNodeCountMinisOne; j++)
+            {
+                if (j != k)
+                {
+                    submat[subi][subj] = laplacian[i][j];
+                    subj++;
                 }
-                subi++;
             }
-            det += (k % 2 == 0 ? 1 : -1) * laplacian[0][k] * determinant(submat, graphNodeCountMinisOne - 1);
+            subi++;
         }
+        // Для каждого столбца k мы вычисляем минор laplacian[0][k] (первый элемент первой строки и k-ый столбец) и умножаем его на соответствующий алгебраический
+        // дополнительный знак (k % 2 == 0 ? 1 : -1).
+        // Затем рекурсивно вызываем функцию determinant для подматрицы submat, умноженной на минор, и добавляем результат к общему определителю det.
+        det += (k % 2 == 0 ? 1 : -1) * laplacian[0][k] * determinant(submat, graphNodeCountMinisOne - 1);
     }
+
     return det;
 }
 
-int main() {
+int main()
+{
     int graphNodeCount;
     ifstream in("test.txt");
     in >> graphNodeCount;
 
     // Создание и инициализация графа
     vector<vector<int>> graph(graphNodeCount, vector<int>(graphNodeCount));
-    for (int i = 0; i < graphNodeCount; i++) {
-        for (int j = 0; j < graphNodeCount; j++) {
+    for (int i = 0; i < graphNodeCount; i++)
+    {
+        for (int j = 0; j < graphNodeCount; j++)
+        {
             in >> graph[i][j];
         }
     }
 
     // Создание лапласиана (http://www.machinelearning.ru/wiki/images/9/9f/Novikov_SGT_presentation.pdf)
     vector<vector<int>> laplacian(graphNodeCount, vector<int>(graphNodeCount, 0));
-    for (int i = 0; i < graphNodeCount; i++) {
-        for (int j = 0; j < graphNodeCount; j++) {
-            if (i != j) {
+    for (int i = 0; i < graphNodeCount; i++)
+    {
+        for (int j = 0; j < graphNodeCount; j++)
+        {
+            if (i != j)
+            {
                 laplacian[i][i] += graph[i][j];
                 laplacian[i][j] = -graph[i][j];
             }
